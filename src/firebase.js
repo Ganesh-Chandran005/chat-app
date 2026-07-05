6ccc69c47e655747819587252d210cb119049d29
Ganesh-Chandran005 <ganeshchandran2028@cs.sjcetpalai.ac.in>
2026-07-05 11:46:34 +0530

    Deploy production code with authentic firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAaqw26SBYCr-WNks5X0hq0Kzc-iZVY5yc",
   authDomain: "chat-app-efaf3.firebaseapp.com",
@@ -8,3 +11,8 @@ const firebaseConfig = {
   appId: "1:900244562691:web:15ed49c0a505b0dac13f36",
   measurementId: "G-EYH8B5SPGW"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
