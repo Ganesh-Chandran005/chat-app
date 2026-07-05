37cc32bde522e8c0ffb526a3d608ad300427f220
Ganesh-Chandran005 <ganeshchandran2028@cs.sjcetpalai.ac.in>
2026-07-05 11:42:13 +0530

    Deploy authentic firebase config API credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAaqw26SBYCr-WNKs5X0hqQKzc-iZVY5yc",
+  apiKey: "AIzaSyAaqw26SBYCr-WNks5X0hq0Kzc-iZVY5yc",
   authDomain: "chat-app-efaf3.firebaseapp.com",
   projectId: "chat-app-efaf3",
   storageBucket: "chat-app-efaf3.firebasestorage.app",
@@ -11,8 +8,3 @@ const firebaseConfig = {
   appId: "1:900244562691:web:15ed49c0a505b0dac13f36",
   measurementId: "G-EYH8B5SPGW"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
